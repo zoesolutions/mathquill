24bf62573312f7abe7afc4efded16c8080a0977a
Daniel Ruthardt <daniel.ruthardt@zoesolutions.eu>
2012-07-26 09:43:02 -0500

    Fixed degree symbol (without curly brackets it isn't parsed back correctly from LaTeX source).

--- src/symbols.js
+++ src/symbols.js
@@ -486,7 +486,7 @@ LatexCmds.cup = LatexCmds.union = bind(BinaryOperator,'\\cup ','&cup;');
 LatexCmds.cap = LatexCmds.intersect = LatexCmds.intersection =
   bind(BinaryOperator,'\\cap ','&cap;');
 
-LatexCmds['°'] = LatexCmds.deg = LatexCmds.degree = bind(VanillaSymbol,'^\\circ ','&deg;');
+LatexCmds['°'] = LatexCmds.deg = LatexCmds.degree = bind(VanillaSymbol,'^{\\circ} ','&deg;');
 
 LatexCmds.ang = LatexCmds.angle = bind(VanillaSymbol,'\\angle ','&ang;');
 
